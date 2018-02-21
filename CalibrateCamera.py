e4408906248622b308ecc42abf381ba3cb035d01
shokah <shokah.s@gmail.com>
2018-02-21 12:59:43 +0200

    start

---
+++ CalibrateCamera.py
@@ -0,0 +1,294 @@
+import numpy as np
+import cv2
+import glob
+from matplotlib import pyplot as plt
+
+def CalibrateCamera(images, rows, cols, numOfImages=15):
+    '''
+    calibrate camera intristic parameters using zhang chess board calibration
+    :param images: list of chess board images paths
+    :param rows: number of inner corner rows in the chess board
+    :param cols: number of inner corner cols in the chess board
+    :param numOfImages: nu,ber of images to use in the calibration
+    :return:
+    '''
+    # termination criteria
+    criteria = (cv2.TERM_CRITERIA_EPS + cv2.TERM_CRITERIA_MAX_ITER, 30, 0.001)
+
+    # prepare object points, like (0,0,0), (1,0,0), (2,0,0) ....,(6,5,0)
+    objp = np.zeros((cols * rows, 3), np.float32)
+    objp[:, :2] = np.mgrid[0:rows, 0:cols].T.reshape(-1, 2)
+
+    # Arrays to store object points and image points from all the images.
+    objpoints = []  # 3d point in real world space
+    imgpoints = []  # 2d points in image plane.
+    c = 1
+    for fname in images:
+        img = cv2.imread(fname)
+        gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+        h, w = gray.shape
+        if h > w : continue
+        # Resize the image to make the algorithm goes faster
+        # later rescale f,xp,yp in the same way,
+        # distortions are not affected by resize
+        gray = cv2.resize(gray, (gray.shape[1]/2, gray.shape[0]/2))
+
+        # Find the chess board corners
+        ret, corners = cv2.findChessboardCorners(gray, (rows, cols), None)
+
+        # If found, add object points, image points (after refining them)
+        if ret == True:
+            objpoints.append(objp)
+
+            # Refine the found corners to sub pixels level
+            corners2 = cv2.cornerSubPix(gray, corners,
+                                        (11, 11), (-1, -1), criteria)
+            imgpoints.append(corners2)
+
+        if c == numOfImages:
+            print c
+            break
+        print c
+        c += 1
+    # rescale the focal length and principal point positions
+    resize_fix = np.array([[2, 0, 2], [0, 2, 2], [0, 0, 1]])
+    ret, mtx, dist, rvecs, tvecs = cv2.calibrateCamera(
+        objpoints, imgpoints, gray.shape[::-1], None, None)
+    return ret, resize_fix.dot(mtx), dist, rvecs, tvecs
+
+def CalibrateProjector(imagesCamera, imagesProjector, imageChessPattern, rows, cols,
+                       cameraMatrix, camDistCoeffs, numOfImages=15):
+    '''
+    preform a chess board calibration for a projector.
+    calibration is done by taking one image of a printed board, covering it,
+    then projecting a chess board and taking it's image (board surface must
+     stay the same for these two images)
+    with the first image the camera pose is found.
+    with the camera pose and camera intrinstic parameters the projected chess
+    board world coordinates
+    can be computed and used for the projector calibration
+    :param imagesCamera: list of camera images paths used for pose estimation
+    :param imagesProjector: list of projector images paths used for calibration
+    :param imageChessPattern: the projected chess pattern image (gray scale)
+    :param rows: number of inner corner rows in the chess board
+    :param cols: number of inner corner cols in the chess board
+    :param cameraMatrix: camera calibration matrix 3x3 numpy array
+    :param camDistCoeffs: camera distortion coeef
+    :param numOfImages: number of images to include in the computation
+    (taking the first n images)
+    :return:
+    '''
+
+    # speed the algorithm - rescale camera matrix to fit the re sized shape
+    cameraMatrix = np.array([[0.5, 0, 0.5], [0, 0.5, 0.5], [0, 0, 1]])\
+        .dot(cameraMatrix)
+    # termination criteria
+    criteria = (cv2.TERM_CRITERIA_EPS + cv2.TERM_CRITERIA_MAX_ITER, 30, 0.001)
+
+    # prepare object points, like (0,0,0), (1,0,0), (2,0,0) ....,(6,5,0)
+    objp = np.zeros((cols * rows, 3), np.float32)
+    objp[:, :2] = np.mgrid[0:rows, 0:cols].T.reshape(-1, 2)
+
+    # Arrays to store object points and image points from all the images.
+    projectorImagepoints = []  # 2d points in image plane.
+    projectorObjPoints = []
+    c = 1
+
+    # Resize the image to make the algorithm goes faster
+    # later rescale f,xp,yp in the same way,
+    # distortions are not affected by resize
+    imageChessPattern = cv2.resize(imageChessPattern, (imageChessPattern.shape[1] / 2,
+                                                       imageChessPattern.shape[0] / 2))
+    # Find the chess board corners in the projector
+    ret_chess, corners_chess = cv2.findChessboardCorners(imageChessPattern, (rows, cols), None)
+    corners_chess = cv2.cornerSubPix(imageChessPattern, corners_chess, (11, 11), (-1, -1), criteria)
+    corners_chess = corners_chess.astype('float32')
+
+    for i in xrange(len(imagesCamera)):
+        # this loop find camera pose per image and calculate the 3d object
+        # points in projector frame
+        img_cam = cv2.imread(imagesCamera[i])
+        gray_cam = cv2.cvtColor(img_cam, cv2.COLOR_BGR2GRAY)
+        h, w = gray_cam.shape
+        if h > w: continue
+        # Resize the image to make the algorithm goes faster
+        # later rescale f,xp,yp in the same way,
+        # distortions are not affected by resize
+        gray_cam = cv2.resize(gray_cam, (gray_cam.shape[1] / 2, gray_cam.shape[0] / 2))
+
+        # Find the chess board corners
+        ret_cam, corners_cam = cv2.findChessboardCorners(gray_cam, (rows, cols), None)
+
+        img_proj = cv2.imread(imagesProjector[i])
+        gray_proj = cv2.cvtColor(img_proj, cv2.COLOR_BGR2GRAY)
+        h, w = gray_proj.shape
+        if h > w: continue
+        # Resize the image to make the algorithm goes faster
+        # later rescale f,xp,yp in the same way,
+        # distortions are not affected by resize
+        gray_proj = cv2.resize(gray_proj, (gray_proj.shape[1] / 2, gray_proj.shape[0] / 2))
+
+        # Find the chess board corners
+        ret_proj, corners_proj = cv2.findChessboardCorners(gray_proj, (rows, cols), None)
+
+        # If found, add object points, image points (after refining them)
+        if ret_cam == True and ret_proj == True:
+
+            # Refine the found corners to sub pixels level
+            imgpoints_cam = cv2.cornerSubPix(
+                gray_cam, corners_cam, (11, 11), (-1, -1), criteria)
+            imgpoints_proj = cv2.cornerSubPix(
+                gray_proj, corners_proj, (11, 11), (-1, -1), criteria)
+
+            # find camera position relative to the printed chess board
+            ret, rvec, tvec = cv2.solvePnP(objp, imgpoints_cam, cameraMatrix, camDistCoeffs)
+
+            # compute object points in projector space using the image pose
+            # relative to calibration board and camera calibration matrix
+            R = cv2.Rodrigues(rvec)[0][:, :2]
+
+            imgpoints_proj = cv2.undistortPoints(imgpoints_proj, cameraMatrix, camDistCoeffs)
+            imgpoints_proj = imgpoints_proj.astype('float32')
+            # Stack the found image points for the projector
+            projectorImagepoints.append(corners_chess)
+            imgpoints_proj = cv2.convertPointsToHomogeneous(imgpoints_proj)
+            imgpoints_proj = np.asarray(np.reshape(imgpoints_proj,
+                                                   (imgpoints_proj.shape[0],
+                                                    imgpoints_proj.shape[2])),
+                                        dtype=np.float32)
+            K_inv = np.linalg.inv(cameraMatrix)
+            Rt_inv = np.linalg.inv(np.hstack((R, tvec)))
+            # calc projected chess board 3d points
+            projPoints = K_inv.dot(Rt_inv).dot(imgpoints_proj.T)
+            projPoints = projPoints.T
+            projPoints = projPoints.astype('float32')
+
+            projectorObjPoints.append(projPoints)
+        if c == numOfImages:
+            print c
+            break
+        print c
+        c += 1
+    initProjCalibMat = np.array([[imageChessPattern.shape[0], 0, imageChessPattern.shape[1]/2],
+                                 [0, imageChessPattern.shape[0], imageChessPattern.shape[0]/2],
+                                [0, 0, 1]], dtype=np.float32)
+    # calibrate the projector based on the images of the projected chess board
+    ret, mtx, dist, rvecs, tvecs = cv2.calibrateCamera(projectorObjPoints,
+                                                       projectorImagepoints,
+                                                       chessboard.shape[::-1],
+                                                       initProjCalibMat,
+                                                       camDistCoeffs,
+                                                       flags=cv2.CALIB_USE_INTRINSIC_GUESS)
+    # rescale the focal length and principal point positions
+    resize_fix = np.array([[2, 0, 2], [0, 2, 2], [0, 0, 1]])
+    return ret, resize_fix.dot(mtx), dist, rvecs, tvecs
+
+def RelativeOrientation(image_cam, image_proj, rows, cols, camMatrix, camDistCoeffs, projMatrix,
+                        projDistCoeffs):
+    '''
+    compute camera relative orientation to the projector
+    :param image_cam: image of the chess board as seen by the camera
+    :param image_proj: image of the projected chessboard
+    :param rows: num of rows in chessboard
+    :param cols: num of cols in chessboard
+    :param camMatrix: camera calibration matrix
+    :param camDistCoeffs: camera distortions
+    :param projMatrix: projector calibration matrix
+    :param projDistCoeffs: projector distortions
+    :return: rotation matrix from camera to projector, translation vector from camera to projector
+    '''
+    # termination criteria
+    criteria = (cv2.TERM_CRITERIA_EPS + cv2.TERM_CRITERIA_MAX_ITER, 30, 0.001)
+
+    # prepare object points, like (0,0,0), (1,0,0), (2,0,0) ....,(6,5,0)
+    objectPoints = np.zeros((cols * rows, 3), np.float32)
+    objectPoints[:, :2] = np.mgrid[0:rows, 0:cols].T.reshape(-1, 2)
+
+    # image points for camera
+    img = cv2.imread(image_cam)
+    gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+
+    # Find the chess board corners
+    ret, corners = cv2.findChessboardCorners(gray, (rows, cols), None)
+
+    # If found, add object points, image points (after refining them)
+    if ret == True:
+
+        # Refine the found corners to sub pixels level
+        imagePoints_cam = cv2.cornerSubPix(gray, corners,
+                                    (11, 11), (-1, -1), criteria)
+
+    # image points for projector
+    img = cv2.imread(image_proj)
+    gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+
+    # Find the chess board corners
+    ret, corners = cv2.findChessboardCorners(gray, (rows, cols), None)
+
+    # If found, add object points, image points (after refining them)
+    if ret == True:
+        # Refine the found corners to sub pixels level
+        imagePoints_proj = cv2.cornerSubPix(gray, corners,
+                                        (11, 11), (-1, -1), criteria)
+
+    stereocalib_retval, cameraMatrix1, distCoeffs1, cameraMatrix2, distCoeffs2, R, T, E, F = \
+        cv2.stereoCalibrate([objectPoints], [imagePoints_proj], [imagePoints_cam],
+                            projMatrix, projDistCoeffs, camMatrix,
+                            camDistCoeffs, gray.shape[::-1], criteria=criteria,
+                            flags=cv2.CALIB_FIX_INTRINSIC)
+    return R, T
+
+
+
+if __name__ == '__main__':
+    images_cam = glob.glob('.\images\calibration\cam\*.jpg')
+    images_proj = glob.glob('.\images\calibration\proj\*.jpg')
+    chess_path = '.\images\chessboard\checkerboard.png'
+    chessboard = cv2.imread(chess_path, 0)
+    chessboard = cv2.resize(chessboard, (800, 600))
+    rows = 6
+    cols = 9
+    # ret, cam_mtx, cam_dist, rvecs, tvecs = CalibrateCamera(
+    #     images_cam, rows, cols, numOfImages=len(images_cam))
+    # print "camera mtx ", cam_mtx
+    # print 80 * '-'
+    # print "camera dist ", cam_dist
+    # print 80 * '-'
+
+    cam_mtx = np.array([[3.52676168e+03, 0.00000000e+00, 3.01315339e+03],
+                        [0.00000000e+00, 3.40613533e+03, 1.86758386e+03],
+                        [0.00000000e+00, 0.00000000e+00, 1.00000000e+00]])
+    cam_dist = np.array([[1.43317881e+00, -1.60413408e+01, -5.02219786e-04, 5.46041307e-02,
+                          8.13172505e+01]])
+
+    # ret, proj_mtx, proj_dist, rvecs, tvecs = CalibrateProjector(
+    #     images_cam, images_proj, chessboard, rows, cols, cam_mtx, cam_dist,
+    #     numOfImages=len(images_proj))
+    # print "projector mtx ", proj_mtx
+    # print 80 * '-'
+    # print "projector dist ", proj_dist
+    # print 80 * '-'
+
+
+
+
+    proj_mtx = np.array([[ 557.32272774,    0.,          434.29075766],
+ [   0.,          599.19032733,  290.08959004],
+ [   0.,            0.,            1.        ]])
+    proj_dist = np.array([[ -1.20178761e-01,   5.02083179e-03,   2.30760632e-03,   4.06847903e-02,
+    1.52240260e-05]])
+    relative = images_cam[0]
+    # relative = "./images/simple/relative.JPG"
+    R, t = RelativeOrientation(relative,
+                               chess_path,
+                               rows,
+                               cols,
+                               cam_mtx,
+                               cam_dist,
+                               proj_mtx,
+                               proj_dist)
+    print "relative rotation matrix\n", R
+    print 80 * '-'
+
+    print "relative translation vector\n", t
+    print 80 * '-'
---
+++ StructuredLight.py
@@ -0,0 +1,252 @@
+import cv2
+import numpy as np
+from matplotlib import pyplot as plt
+import glob
+
+
+# git password - sls123
+
+def GenerateGrayCodePatternsImages(width, height, resultWidth, resultHeight,
+                                   direction='vertical', layerLimit = 20):
+    '''
+    generate a set of binary gray code patterns in the given direction based on projector resolution
+    :param width: projector width in pixels
+    :param height: projector height in pixels
+    :param resultWidth: screen resolution width in pixels
+    :param resultHeight: screen resolution in pixels
+    :param direction: vertical or horizontal lines (as string)
+    :return: list of pattern images
+    '''
+    patterns = []
+    if direction == 'vertical':
+        max_lvl = int(np.log2(width))
+        p = np.array([[0, 255]])
+    else:
+        max_lvl = int(np.log2(height))
+        p = np.array([0, 255])
+    patterns.append(p)
+    i = 1
+    max_lvl = np.min([max_lvl, layerLimit])
+    # goes over each level and create the image
+    while i <= max_lvl:
+        cv2.imwrite(str(i)+'.jpg', p)
+        img = cv2.imread(str(i)+'.jpg', 0)
+        img = cv2.resize(img, (resultWidth, resultHeight))
+        ret, img = cv2.threshold(img, 127, 255, cv2.THRESH_BINARY)
+        cv2.imwrite(str(i)+'.jpg', img)
+        if direction == 'vertical':
+            p = np.hstack((p, np.array(patterns[i-1][:, ::-1])))
+        else: # not fully working, make sure to fix it
+            p = np.hstack((p, np.array(patterns[i - 1][:-1])))
+        patterns.append(p)
+        i += 1
+    return patterns
+
+
+def BuildCodeMatrix(pathes, mode='pattern'):
+    '''
+    stack the images, the stacked matrix gives the binary code in x,y cell
+    :param pathes: list of images paths
+    :return: numpy array of height x width x depth(number of images)
+    '''
+    code_map = 0
+    c = 0
+    if mode == 'pattern':
+        thresh_min = 127
+        thresh_max = 255
+    else:
+        thresh_min = 100
+        thresh_max = 200
+    for p in pathes:  # this loops build the planes code map
+        img = cv2.imread(p, 0)
+        ret, img = cv2.threshold(img, thresh_min, thresh_max, cv2.THRESH_BINARY)
+        # plt.imshow(img)
+        # plt.show()
+        if c != 0:  # other iterations
+            code_map[:, :, c] = img
+        else:    # first iteration
+            rows, cols, depth = img.shape[0], img.shape[1], len(pathes)
+            code_map = np.zeros((rows, cols, depth), dtype=int)
+            code_map[:, :, c] = img
+        c += 1
+    code_map[code_map > 0] = 1   # make the map binary
+    return code_map
+
+def BuildLightPlanes(paths, projCalibMatrix, distCoeffs, distBetweenPlanes = 2):
+    '''
+    build the light planes normals that goes from the projector perspective center
+    :param pathes: list of patterns paths (vertical)
+    :param projCalibMatrix: projector calibration matrix
+    :param distBetweenPlanes: gap between light planes
+    :return:
+    '''
+    xp, yp, projectorFocalLength = projCalibMatrix[0, 2], projCalibMatrix[1, 2], \
+                                   (projCalibMatrix[0, 0] + projCalibMatrix[1, 1]) * 0.5
+    planes = {}
+    mat = BuildCodeMatrix(paths)
+    perspective = np.hstack((projCalibMatrix, np.reshape([0, 0, 0], (3, 1))))
+
+    for i in xrange(0, mat.shape[1], distBetweenPlanes):
+        key = "".join(map(str, mat[int(yp), i]))
+        # undistort the points that build the light plane
+        # top point
+        p = np.array([[[-yp, i]]], dtype=np.float32)
+        p = cv2.undistortPoints(p, projCalibMatrix, distCoeffs, R=np.eye(3), P=perspective)
+        x, y1 = p[0, 0, 0], p[0, 0, 1]
+        v1 = np.array([x, y1, -projectorFocalLength])
+        # bot point
+        p = np.array([[[yp, i]]], dtype=np.float32)
+        p = cv2.undistortPoints(p, projCalibMatrix, distCoeffs, R=np.eye(3), P=perspective)
+        x, y2 = p[0, 0, 0], p[0, 0, 1]
+        v2 = np.array([x, y2, -projectorFocalLength])
+        # old version - without distortions - keep it might need it later
+        # x, y1, y2 = i - xp, -yp, yp
+        # v1 = np.array([x, y1, -projectorFocalLength])
+        # v2 = np.array([x, y2, -projectorFocalLength])
+        n = np.cross(v1, v2)
+        planes[key] = n/np.linalg.norm(n)
+    return planes
+
+def PlaneAndRayIntersection(planeOrigin, rayOrigin, planeNormal, rayDirection):
+    '''
+    compute intersection point of a line ray and a plane
+    :param planeOrigin: plane's origin as numpy array 1x3
+    :param rayOrigin: ray's origin as numpy array 1x3
+    :param planeNormal: plane's normal as numpy array 1x3
+    :param rayDirection: ray's direction as numpy array 1x3
+    :return: 3d point as numpy array 1x3
+    '''
+    a = planeOrigin - rayOrigin
+    b = rayDirection.dot(planeNormal)
+    angle = np.degrees(np.arccos(b/(np.linalg.norm(rayDirection) * np.linalg.norm(planeNormal))))
+    if 75 < angle < 105:  # in case of ray and light plane are parallel
+        return None
+    c = a.dot(planeNormal)
+    d = c * (rayDirection)
+    return rayOrigin + (d / b)
+
+def ComputePointsCloud(lightPlanes, camCodeMatrix, projPosition,
+                       camCalibMatrix, distCoeffs, camPosition,
+                       camRotation, startPoint, endPoint, distanceBetweenRays = 2):
+    '''
+    compute all the 3d points in the scene
+    :param lightPlanes: dictionary of light planes normals
+    :param camCodeMatrix: binary code matrix for each pixel (height x width x depth)
+    :param camCalibMatrix: camera calibration  3x3 numpy array
+    :param distCoeffs: opencv distortions coeffs
+    :param camPosition: camera position relative to the projector as 1x3 numpy array
+    :param camRotation: camera rotation relative to the projector as 3x3 numpy array
+    :param startPoint: start scan point (row, col) as tuple
+    :param endPoint: end scan point (row, col) as tuple
+    :return: list of XYZ points
+    '''
+
+    xp, yp, camFocalLength = camCalibMatrix[0, 2], camCalibMatrix[1, 2],\
+                           (camCalibMatrix[0, 0] + camCalibMatrix[1, 1]) * 0.5
+    points = []
+    startRow = startPoint[0]
+    endRow = endPoint[0]
+    startCol = startPoint[1]
+    endCol = endPoint[1]
+    perspective = np.hstack((camCalibMatrix, np.reshape(camPosition, (3, 1))))
+    for i in xrange(startRow, endRow, distanceBetweenRays):
+        for j in xrange(startCol, endCol, distanceBetweenRays):
+            # # un distort the image point
+            # p = np.array([[[j, i]]], dtype=np.float32)
+            # p = cv2.undistortPoints(p, camCalibMatrix, distCoeffs, R=camRotation, P=perspective)
+            # x, y = p[0, 0, 0], p[0, 0, 1]
+            x, y = j - xp, -(i - yp)    # without distortion fix
+            rayDirection = camRotation.dot(np.array([x, y, -camFocalLength]))
+            rayDirection /= np.linalg.norm(rayDirection)
+            key = "".join(map(str, camCodeMatrix[i, j]))
+            if int(key) == 0:   # remove background
+                continue
+            try:
+                planeNormal = lightPlanes[key]
+                p = PlaneAndRayIntersection(projPosition, camPosition, planeNormal, rayDirection)
+                if p is None:
+                    continue
+                points.append(p)
+            except:
+                pass
+    return points
+
+def WritePointsToFile(points, file_name, format='txt'):
+    '''
+    write the cloud points to the file in the given format
+    :param points: nx3 list of points
+    :param file_name: name of file
+    :param format: file format - support only text now
+    :return: None
+    '''
+    text_file = open(file_name + "." + format, "w")
+    for p in points:
+        str1 = str(p[0]) + "," + str(p[1]) + "," + str(p[2]) + "\n"
+        text_file.write(str1)
+    text_file.close()
+
+def checkBitChange(prev_key, key):
+    '''
+    check if the difference in bit's between the keys is 1 return true if so
+    :param prev_key: previous key
+    :param key: current key
+    :return:  bool
+    '''
+    tot = 0
+    for i in xrange(len(prev_key)):
+        d = np.abs(int(key[i]) - int(prev_key[i]))
+        tot += d
+    return tot == 1
+
+
+if __name__ == '__main__':
+    # GenerateGrayCodePatternsImages(800, 600, 800, 600)
+    # load image to search targets in it
+    patterns_paths = glob.glob('.\images\pattern\*.jpg')
+    box1_paths = glob.glob('.\images\simple\\box\*.jpg')
+    box2_paths = glob.glob('.\images\simple\\box2\*.jpg')
+    binder_paths = glob.glob('.\images\simple\\binder\*.jpg')
+    dummie_paths = glob.glob('.\images\\dummie\*.jpg')
+
+    proj_mtx = np.array([[557.32272774, 0., 434.29075766],
+                         [0., 599.19032733, 290.08959004],
+                         [0., 0., 1.]], dtype=np.float32)
+    proj_dist = np.array([[-1.20178761e-01, 5.02083179e-03, 2.30760632e-03, 4.06847903e-02,
+                           1.52240260e-05]], dtype=np.float32)
+    print "building light planes..."
+    planes = BuildLightPlanes(patterns_paths, proj_mtx, proj_dist, distBetweenPlanes=8)
+
+    projPosition = np.array([0, 0, 0])
+
+    cam_mtx = np.array([[3.52676168e+03, 0.00000000e+00, 3.01315339e+03],
+                        [0.00000000e+00, 3.40613533e+03, 1.86758386e+03],
+                        [0.00000000e+00, 0.00000000e+00, 1.00000000e+00]], dtype=np.float32)
+    cam_dist = np.array([[1.43317881e+00, -1.60413408e+01, -5.02219786e-04, 5.46041307e-02,
+                          8.13172505e+01]], dtype=np.float32)
+    camPosition_box = np.array([ -4.54306512, -4.84217478, 23.56067222], dtype=np.float32)
+    camRotation_box = np.array([[ 9.85955787e-01,   5.38614856e-02,   1.58082660e-01],
+ [ -2.16918651e-04,   9.46977857e-01,  -3.21298756e-01],
+ [ -1.67006407e-01,   3.16752077e-01,   9.33689982e-01]], dtype=np.float32)
+
+    camPosition_dum = np.array([-9.85938852,-1.99040076, 39.25014791], dtype=np.float32)
+    camRotation_dum = np.array([[ 0.97130392,  0.03227768,  0.23564135],
+ [-0.01083326,  0.99572424, -0.09173807],
+ [-0.2375949,   0.08655278,  0.96750053]],
+                               dtype=np.float32)
+    camCodeMatrix = BuildCodeMatrix(dummie_paths, 'map')
+    print "building camera code matrix..."
+    # camCodeMatrix = BuildCodeMatrix(box1_paths, 'map')
+
+    # full image
+    startPoint = [0, 0]
+    endPoint = camCodeMatrix.shape[:2]
+    print "compute point cloud..."
+    points = ComputePointsCloud(planes, camCodeMatrix, projPosition,
+                                cam_mtx, cam_dist, camPosition_dum, camRotation_dum,
+                                startPoint, endPoint, distanceBetweenRays=8)
+    # points = ComputePointsCloud(planes, camCodeMatrix, projPosition,
+    #                             cam_mtx, cam_dist, camPosition_box, camRotation_box,
+    #                             startPoint, endPoint, distanceBetweenRays=16)
+    print "write points to file..."
+    WritePointsToFile(points, "dummie_undistorted_proj", format='txt')
+    print "DONE!"
